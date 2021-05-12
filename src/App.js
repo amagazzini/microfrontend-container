c6313f054d2095c91e7b7e3fb76b2e248d659cdc
Andrea Magazzini <anz@ti8m.ch>
2021-05-12 18:33:54 +0400

    remove unused app from course

--- src/App.js
+++ src/App.js
@@ -1,50 +1,20 @@
-import React, { lazy, Suspense, useState, useEffect } from 'react';
-import { Router, Route, Switch, Redirect } from 'react-router-dom';
-import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
+import React, { lazy, Suspense } from 'react';
+import { Router, Route, Switch } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
 
 import Progress from './components/Progress'
-import Header from './components/Header';
-
-// const MarketingLazy = lazy(() => import('./components/MarketingApp'));
-// const AuthLazy = lazy(() => import('./components/AuthApp'));
-// const DashboardLazy = lazy(() => import('./components/DashboardApp'))
 const MonoreporeactLazy = lazy(() => import('./components/MonoreporeactApp'));
-
-const generateClassName = createGenerateClassName({
-  productionPrefix: 'co',
-})
 
 const history = createBrowserHistory();
 
 export default () => {
-  const [isSignedIn, setIsSignedIn] = useState(false);
-
-  useEffect(() => {
-    if(isSignedIn) {
-      history.push('/dashboard');
-    }
-  }, [isSignedIn])
-
   return (
     <Router history={history}>
-      <StylesProvider generateClassName={generateClassName}>
-        <div>
-          {/* <Header onSignOut={() => setIsSignedIn(false)} isSignedIn={isSignedIn} /> */}
-          <Suspense fallback={<Progress />}>
-            <Switch>
-              {/* <Route path="/auth">
-                <AuthLazy onSignIn={() => setIsSignedIn(true)} />
-              </Route>
-              <Route path="/dashboard">
-                {!isSignedIn && <Redirect to="/" />}
-                <DashboardLazy />
-              </Route> */}
-              <Route path="/" component={MonoreporeactLazy} />
-            </Switch>
-          </Suspense>
-        </div>
-      </StylesProvider>
+      <Suspense fallback={<Progress />}>
+        <Switch>
+          <Route path="/" component={MonoreporeactLazy} />
+        </Switch>
+      </Suspense>
     </Router>
   );
 }
